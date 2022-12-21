db1499fc9004f575b00985cb1d745a695939f529
j-tee <juliustetteh@gmail.com>
2022-12-21 13:10:38 +0000

    Set up associations

--- app/person.rb
+++ app/person.rb
@@ -1,5 +1,9 @@
+require_relative 'rental'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :age, :name, :parent_permission
+  attr_accessor :age, :name, :parent_permission, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -8,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,9 +23,19 @@ class Person < Nameable
     @name
   end
 
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
